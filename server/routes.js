eddd4597d1c41a06af5be49a68dcb166c04970ed
Steven Lundy <steven.lundy@gmail.com>
2016-01-02 22:04:31 -0500

    Add REST API routes for get

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,30 @@
 var router = require('express').Router();
+var models = require('./models');
 
-router.get('/test', function(req, res) {
-  res.send('this is a test!');
-})
+router.get('/patrons', function(req, res) {
+  models.patrons.getAll().then(function(patrons) {
+    res.status(200).send(patrons);
+  }).catch(function(err) {
+    res.status(400).send(err);
+  });
+});
+
+
+router.get('/items', function(req, res) {
+  models.items.getAll().then(function(items) {
+    res.status(200).send(items);
+  }).catch(function(err) {
+    res.status(400).send(err);
+  });
+});
+
+
+router.get('/orders', function(req, res) {
+  models.orders.getAll().then(function(orders) {
+    res.status(200).send(orders);
+  }).catch(function(err) {
+    res.status(400).send(err);
+  });
+});
 
 module.exports = router;
