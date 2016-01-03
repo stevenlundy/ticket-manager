16776f99ea0b625c8170c308bc0376ccfc0141d4
Steven Lundy <steven.lundy@gmail.com>
2016-01-02 22:16:39 -0500

    Add REST API routes for post

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,13 @@ router.get('/patrons', function(req, res) {
     res.status(400).send(err);
   });
 });
+router.post('/patrons', function(req, res) {
+  models.patrons.insert(req.body).then(function(patrons) {
+    res.status(201).send('Created');
+  }).catch(function(err) {
+    res.status(400).send(err);
+  });
+});
 
 
 router.get('/items', function(req, res) {
@@ -17,11 +24,25 @@ router.get('/items', function(req, res) {
     res.status(400).send(err);
   });
 });
+router.post('/items', function(req, res) {
+  models.items.insert(req.body).then(function(items) {
+    res.status(201).send('Created');
+  }).catch(function(err) {
+    res.status(400).send(err);
+  });
+});
 
 
 router.get('/orders', function(req, res) {
   models.orders.getAll().then(function(orders) {
     res.status(200).send(orders);
+  }).catch(function(err) {
+    res.status(400).send(err);
+  });
+});
+router.post('/orders', function(req, res) {
+  models.orders.insert(req.body).then(function(orders) {
+    res.status(201).send('Created');
   }).catch(function(err) {
     res.status(400).send(err);
   });
